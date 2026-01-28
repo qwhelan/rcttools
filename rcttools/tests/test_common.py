9e3c244a1c32a3ae7d176ace814d54418cef230e
Christopher Whelan <topherwhelan@gmail.com>
2026-01-27 21:27:45 -0800

    Add heading to output, assuming RCT715 is pointed backwards

--- rcttools/tests/test_common.py
+++ rcttools/tests/test_common.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 import pytest
 
-from ..common import dec_to_dms
+from ..common import calc_bearing, dec_to_dms
 
 
 @pytest.mark.parametrize(
@@ -21,3 +21,36 @@ def test_dec_to_dms(
 ) -> None:
     result = dec_to_dms(decimal_degrees)
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "lat1, lon1, lat2, lon2, expected_bearing",
+    [
+        (Decimal("0"), Decimal("0"), Decimal("1"), Decimal("0"), Decimal("0.000000")),
+        (Decimal("0"), Decimal("0"), Decimal("0"), Decimal("1"), Decimal("90.000000")),
+        (
+            Decimal("0"),
+            Decimal("0"),
+            Decimal("0.1"),
+            Decimal("0.1"),
+            Decimal("44.999956"),
+        ),
+        (
+            Decimal("0"),
+            Decimal("0"),
+            Decimal("-1"),
+            Decimal("0"),
+            Decimal("180.000000"),
+        ),
+        (Decimal("0"), Decimal("0"), Decimal("0"), Decimal("0"), None),
+    ],
+)
+def test_calc_bearing(
+    lat1: Decimal,
+    lon1: Decimal,
+    lat2: Decimal,
+    lon2: Decimal,
+    expected_bearing: Decimal,
+) -> None:
+    result = calc_bearing(lat1, lon1, lat2, lon2)
+    assert result == expected_bearing
